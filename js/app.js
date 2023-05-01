e669358ab8fb0c6e60ddd23f3ae372c2d9141123
Genivania <109962733+Genivania@users.noreply.github.com>
2023-05-01 12:38:24 -0300

    inicio ok

---
+++ js/app.js
@@ -0,0 +1,23 @@
+'use strict'
+
+const routes = {
+    '/': '/pages/inicio.html',
+    '/historia': '/pages/historia.html',
+    '/personagens': '/pages/personagens.html',
+    '/episodios': '/pages/episodios.html'
+}
+
+const route = async() => {
+    window.event.preventDefault()
+    window.history.pushState({}, "", window.event.target.href)
+
+    const path = window.location.pathname
+    const route = routes[path]
+
+    const response = await fetch(route)
+    const html = await response.text()
+
+    document.getElementById('root').innerHTML = html
+}
+
+window.route = route
